456993dd0b1c862720a2783db9b0bc43ec7f0cee
Furkan Sahin <aliince.000000@gmail.com>
2024-07-29 16:17:05 +0300

    Updated - 29.07.2024

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let previewContainer = document.querySelector(".products-preview");
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.addEventListener('click', () => {
+    previewContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target');
+      if (name == target) {
+        preview.classList.add('active');
+      }
+    });
+  });
+});
+
+previewBox.forEach(close => {
+  close.querySelector('.bi-x-lg').addEventListener('click', () => {
+    close.classList.remove('active');
+    previewContainer.style.display = 'none';
+  });
+});
