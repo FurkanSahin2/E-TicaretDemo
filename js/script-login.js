216399e3fd1fc404d65c1306887f42ae40febffb
Furkan Sahin <aliince.000000@gmail.com>
2024-07-29 19:45:20 +0300

    Updated- 29.07.2024

---
+++ js/script-login.js
@@ -0,0 +1,45 @@
+function yonlendir() {
+    var kullaniciAdi = document.getElementById("kullaniciAdi").value;
+    var sifre = document.getElementById("sifre").value;
+
+    if (kullaniciAdi === "e-ticaretdemo" && sifre === "demo123") {
+      window.location.href = "indexx.html";
+    } else {
+      var errorDiv = document.createElement("div");
+      errorDiv.className = "error-message";
+      errorDiv.textContent = "Girilen bilgiler eksik veya hatalı!";
+      errorDiv.style.color = "red";
+      errorDiv.style.fontWeight = "bold";
+      errorDiv.style.fontSize = "20px";
+      errorDiv.style.width = "28%";
+      errorDiv.style.marginLeft = "auto";
+      errorDiv.style.marginRight = "auto";
+      errorDiv.style.textAlign = "center";
+      errorDiv.style.border = "1px solid red";
+      errorDiv.style.padding = "10px";
+      var formContainer = document.querySelector(".form-container");
+      formContainer.appendChild(errorDiv);
+    }
+
+    setTimeout(function () {
+      errorDiv.style.display = "none";
+    }, 2500);
+  }
+
+
+  const options = {
+    bottom: "32px", // default: '32px'
+    right: "32px", // default: '32px'
+    left: "unset", // default: 'unset'
+    time: "0.4s", // default: '0.3s'
+    mixColor: "#fff", // default: '#fff'
+    backgroundColor: "#fff", // default: '#fff'
+    buttonColorDark: "#100f2c", // default: '#100f2c'
+    buttonColorLight: "#fff", // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: "🌓", // default: ''
+    autoMatchOsTheme: true, // default: true
+  };
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
